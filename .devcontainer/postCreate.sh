d8bc79422cfc78ac6a12109384c83a2e56bc3d4c
Lucas Quitman <contact@lucasquitman.fr>
2023-07-23 21:37:07 +0000

    fixing devcontainer permissions

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # inject the sops key if running locally
 if [ -z "$CODESPACE_NAME" ]; then
-    read -p -s 'Enter sops AGE private key: ' SOPS_AGE_KEY
+    read -s -p 'Enter sops AGE private key: ' SOPS_AGE_KEY
 fi
-echo $SOPS_AGE_KEY > /root/.sops/key.txt
+echo $SOPS_AGE_KEY > /home/vscode/.sops/key.txt
 
 cd loren-server && npm install
