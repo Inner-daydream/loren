14eed9afd6e2ba777be2859fcb201062bb710337
Lucas Quitman <contact@lucasquitman.fr>
2023-07-23 21:26:22 +0200

    fix local dev container secret injection

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,5 @@
 # check if running in codespace
 if [ -z "$CODESPACE_NAME" ]; then
     read -p 'Enter sops AGE private key: ' SOPS_AGE_KEY
-    else 
-    echo ""
 fi
-echo $sops_key > /root/.sops/key.txt
+echo $SOPS_AGE_KEY > /root/.sops/key.txt
