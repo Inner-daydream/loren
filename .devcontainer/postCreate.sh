89d5537b7b3a53014ab992f118d71a287fd7646a
Lucas Quitman <contact@lucasquitman.fr>
2023-07-23 21:35:32 +0200

    auto install npm dependencies

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# check if running in codespace
+# inject the sops key if running locally
 if [ -z "$CODESPACE_NAME" ]; then
     read -p 'Enter sops AGE private key: ' SOPS_AGE_KEY
 fi
 echo $SOPS_AGE_KEY > /root/.sops/key.txt
+
+cd loren-server && npm install
