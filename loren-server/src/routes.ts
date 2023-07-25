bf2b08951f4b55c9e5439352266fa580895e1ebd
Lucas Quitman <contact@lucasquitman.fr>
2023-07-25 07:45:23 +0000

    adding prisma

--- loren-server/src/routes.ts
+++ loren-server/src/routes.ts
@@ -1,9 +1,6 @@
 import { Router } from 'express';
 export const router = Router();
 import type { AuthRequest } from './types/index.d.ts';
-// router.get('/', (req, res) => {
-//     res.send('HELLO ZA WARUDO!');
-//   });
 
 router.get('/', (req: AuthRequest, res) => {
   res.send(req.oidc?.isAuthenticated() ? 'Logged in' : 'Logged out');
