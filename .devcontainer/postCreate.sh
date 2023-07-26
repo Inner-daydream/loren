f1be3c0d1d0fe98ac3f98d4e4a9a732ebb737b6d
Lucas Quitman <contact@lucasquitman.fr>
2023-07-26 15:04:56 +0200

    default rebase

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,5 +6,6 @@ if [ -z "$CODESPACE_NAME" ]; then
 fi
 echo $SOPS_AGE_KEY > /home/vscode/.sops/key.txt
 WORKSPACE_ROOT=$(pwd)
+git config pull.rebase true
 cd $WORKSPACE_ROOT/loren-server && npm install
 cd $WORKSPACE_ROOT/infra/terraform && direnv exec . terraform init
