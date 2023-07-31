551517e79b6da3b1fbf9da1f6372df5df3313611
Manoel NUNES BORBA <76746287+manoelnunesborba@users.noreply.github.com>
2023-07-31 16:57:09 +0000

    School and error handler routes

--- loren-server/src/routes.ts
+++ loren-server/src/routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express';
 export const router = Router();
 import type { AuthRequest } from './types/index.d.ts';
 import { UserController } from './controller/user';
-import { PaymentController } from './controller/payment';
 import { authorizationMiddleware } from './middlewares/auth';
+import { PaymentController } from './controller/payment';
+import { SchoolController } from './controller/school';
+
 import { ROLES } from './services/constants';
+import { errorHandler } from './middlewares/error';
+import DefaultResponse from './DTOs/DefaultResponse';
 router.get('/', (req: AuthRequest, res) => {
   res.send(req.oidc?.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
@@ -15,7 +19,10 @@ router.get('/exemple', authorizationMiddleware([ROLES.ADMIN]), (req, res, next) 
 })
 
 router.post('/user', UserController.create);
-
 router.post('/payment/checkout', PaymentController.createCheckoutSession);
 router.post('/payment/checkout/success', PaymentController.handleSuccessfulCheckout);
 router.post('/payment/checkout/expired', PaymentController.handleExpiredSubscriptions);
+
+router.post('/school', SchoolController.create);
+
+router.use(errorHandler());
