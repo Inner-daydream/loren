a642816297fd73d91640cb96297790bfa120f7c3
Manoel NUNES BORBA <76746287+manoelnunesborba@users.noreply.github.com>
2023-08-02 15:30:15 +0000

    seed file

--- loren-server/prisma/seed.ts
+++ loren-server/prisma/seed.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { UserService } from '../src/services/user'
 import { randomUUID } from 'crypto'
-import { ROLES } from '../src/services/constants'
+import { ROLES } from '../src/constants'
 import { ManagementClient } from 'auth0';
 import { ALL } from 'dns';
 import { cp } from 'fs';
+import { SchoolService } from '../src/services/school';
 const management = new ManagementClient({
     domain: process.env.AUTH0_DOMAIN as string,
     token: process.env.AUTH0_TOKEN as string,
@@ -14,46 +15,63 @@ async function main() {
     const jacques = {
         email: 'jacques@loren.app',
         password: randomUUID(),
-        role: ROLES.ADMIN,
     }
     const jeanne = {
         email: 'jeanne@loren.app',
         password: randomUUID(),
-        role: ROLES.STUDENT,
     }
 
     const jean = {
         email: 'jean@loren.app',
         password: randomUUID(),
-        role: ROLES.TEACHER,
     }
+    const school = {
+        "name": "MySchool",
+        "phone": "<Phone number>"
+    }
+    let invitRoleSTUDENT: Promise<string>;
+    let invitRoleTEACHER: Promise<string>;
+
     const res1 = management.getUsersByEmail(jacques.email, function (err, users) {
+        let createUser;
         if (users.length == 0) {
-            UserService.create(jacques.email, jacques.password, jacques.role);
+            createUser = UserService.create(jacques.email, jacques.password);
             console.log("User jacques recreated")
 
         } else {
-            users.map(val => {
+            users?.map(val => {
                 if (val.user_id != undefined) {
                     management.deleteUser({ id: val.user_id }, function (err) {
                         if (err) {
                             console.error("anable to delete")
                         }
                         console.log("User jacques recreated")
-                        UserService.create(jacques.email, jacques.password, jacques.role);
+                        createUser = UserService.create(jacques.email, jacques.password);
                         // User deleted.
                     });
                 }
 
             })
-
         }
+        if (!createUser) {
+            throw new Error("");
+        }
+        createUser.then(x => {
+            const school = SchoolService.create(x.id, "school_1", "number_1");
+            school.then(y => {
+                invitRoleSTUDENT = SchoolService.generateInvite(y.id, ROLES.STUDENT);
+                invitRoleTEACHER = SchoolService.generateInvite(y.id, ROLES.TEACHER);
+            })
+
+        })
+
 
 
     });
     management.getUsersByEmail(jeanne.email, function (err, users) {
         if (users.length == 0) {
-            UserService.create(jeanne.email, jeanne.password, jeanne.role);
+
+            invitRoleSTUDENT.then(x => UserService.create(jeanne.email, jeanne.password, x));
             console.log("User jeanne recreated")
 
         } else {
@@ -64,7 +82,7 @@ async function main() {
                             console.error("anable to delete")
                         }
                         console.log("User jeanne recreated")
-                        UserService.create(jeanne.email, jeanne.password, jeanne.role);
+                        invitRoleSTUDENT.then(x => UserService.create(jeanne.email, jeanne.password, x));
                         // User deleted.
                     });
                 }
@@ -75,7 +93,8 @@ async function main() {
     });
     management.getUsersByEmail(jean.email, function (err, users) {
         if (users.length == 0) {
-            UserService.create(jean.email, jean.password, jean.role);
+            invitRoleTEACHER.then(x => UserService.create(jean.email, jean.password, x));
+
             console.log("User jean recreated")
 
         } else {
@@ -86,7 +105,7 @@ async function main() {
                             console.error("anable to delete")
                         }
                         console.log("User jean recreated")
-                        UserService.create(jean.email, jean.password, jean.role);
+                        invitRoleTEACHER.then(x => UserService.create(jean.email, jean.password, x));
                         // User deleted.
                     });
                 }
